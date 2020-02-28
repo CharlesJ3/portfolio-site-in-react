a65c624e3e5898ad08f8901eca8da33ff27608fb
Charles Jones <c.jones.3.zero@gmail.com>
2020-02-27 21:27:59 -0700

    updating h10 heroku p4

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 3000;
 const app = express();
+
+
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -15,4 +17,6 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
